cf1e7f008ace0d00e5a6faca3a6aa01bf2960a2c
hongwei0417 <iamhongwei0417@gmail.com>
2020-10-15 15:42:47 +0800

    更新重製script

--- backend/reset.js
+++ backend/reset.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import UserClass from './models/user_class_model'
 import UserStock from "./models/user_stock_model";
 import UserTxn from "./models/user_txn_model";
 import Account from "./models/account_model";
@@ -8,16 +9,23 @@ require("dotenv").config();
 
 //重置程序開始
 const Run = () => {
+	if(!['CLASS1','CLASS2','CLASS3'].includes(process.argv[2])) {
+		console.log("錯誤參數")
+		process.exit()
+	}
+	const class_id = process.argv[2];
 	const connection = mongoose.connection;
 	connection.once("open", async() => {
 		console.log("MongoDB database connection established successfully");
         console.log("The database is " + connection.name);
         
         //取得所有使用者
-        const all_users = await User.find().exec();
-		await reset_account(all_users)
-		await reset_user_stock()
-		await reset_transactions()
+		const all_users = await UserClass.find({ class_id }).lean().exec();
+		const all_user_ids = all_users.map(item => item.user)
+		console.log(all_user_ids)
+		await reset_account(all_user_ids)
+		await reset_user_stock(all_user_ids)
+		await reset_transactions(all_user_ids)
 		process.exit();
 	});
 	mongoose.set("useFindAndModify", false);
@@ -29,32 +37,42 @@ const Run = () => {
 };
 
 //重置帳戶
-const reset_account = async(users) => {
-    for(let i = 0; i < users.length; i++) {
-		const updateTime = moment().toDate()
-        await Account.findOneAndUpdate({user: users[i]._id}, {
-			balance: 2000000,
-			total_amount: 0,
-			stock_number: 0,
-			stock_value: 0,
-			txn_count: 0,
-			last_update: updateTime,
-			last_value_update: updateTime,
-			updatedAt:  updateTime,
-		})
-	}
+const reset_account = async(ids = []) => {
+	const updateTime = moment().toDate()
+	await Account.updateMany({
+		user: {
+			$in: ids
+		}
+	},  {
+		balance: 2000000,
+		total_amount: 0,
+		stock_number: 0,
+		stock_value: 0,
+		txn_count: 0,
+		last_update: updateTime,
+		last_value_update: updateTime,
+		updatedAt:  updateTime,
+	}).exec()
 	console.log("所有帳戶重置完成!")
 }
 
 //重置用戶擁有股票
-const reset_user_stock = async() => {
-	await UserStock.deleteMany({}).exec()
+const reset_user_stock = async(ids = []) => {
+	await UserStock.deleteMany({
+		user: {
+			$in: ids
+		}
+	}).exec()
 	console.log("重置所有用戶擁有股票完成!")
 }
 
 //重置所有交易
-const reset_transactions = async() => {
-	await UserTxn.deleteMany({}).exec()
+const reset_transactions = async(ids = []) => {
+	await UserTxn.deleteMany({
+		user: {
+			$in: ids
+		}
+	}).exec()
 	console.log("重置所有交易完成!")
 }
 
